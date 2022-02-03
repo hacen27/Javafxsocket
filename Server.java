a7056a1af664c782eb9c1942b6d2dd5c02f1d799
hacen27 <83768631+hacen27@users.noreply.github.com>
2022-02-03 23:09:37 +0000

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,32 @@
+package application;
+
+
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+
+public class Server extends NetWorkConnection{
+
+    private int port;
+
+    public Server(int port, Consumer<Serializable> onReceiveCallBack) {
+        super(onReceiveCallBack);
+        this.port = port;
+    }
+
+    @Override
+    protected boolean isServer() {
+        return true;
+    }
+
+    @Override
+    protected String getIP() {
+        return null;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+}
+
